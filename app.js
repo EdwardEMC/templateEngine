8db90439d8adf4ec1dec3e069619dfe7c8da035a
EMC <edwardcoad.ec@gmail.com>
2020-01-17 10:34:59 +1030

    role templates complete, questions complete and working

--- app.js
+++ app.js
@@ -14,21 +14,27 @@
 
 //generate the html
 
+//installed packages
 const inquirer = require("inquirer");
+const fs = require("fs");
 
+//class files
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
 const roles = [Manager, Engineer, Intern];
+const teamMembers = [];
 
-let x = 0;
+let x = 0; //counter for question display/array
+
 const questions = [
     {
         name: "What is the manager's name?",
         id: "What is the manager's Id?",
         email: "Please enter the manager's email address:",
         unique: "Please enter the manager's office number:",
+        role: "Manager",
         input: ["name", "id", "email", "unique"] //unique = office
     },
     {
@@ -36,6 +42,7 @@ const questions = [
         id: "What is the engineer's Id?",
         email: "Please enter the engineer's email address:",
         unique: "Please enter the engineer's github username:",
+        role: "Engineer",
         input: ["name", "id", "email", "unique"] //unique = github
     },
     {
@@ -43,6 +50,7 @@ const questions = [
         id: "What is the intern's Id?",
         email: "Please enter the intern's email address:",
         unique: "Please enter the intern's school:",
+        role: "Intern",
         input: ["name", "id", "email", "unique"] //unique = school
     }
 ];
@@ -69,10 +77,18 @@ const question = () => {
         message: questions[x].unique,
         name: questions[x].input[3]
     }]).then(function(res) {
-        console.log(res);
-        //area to put code into html file and save to variable
-        const person = new roles[x](res.name, res.id, res.email, res.unique);
-        console.log(person);
+
+        const person = new roles[x](res.name, res.id, questions[x].role, res.email, res.unique); //still need to fix email and title????????
+
+        teamMembers.push(person); //add the new team member to the teamMembers array
+
+        if(person.getRole() !== "Manager") {
+            askAgain();
+        }
+        else {
+            x++;
+            question();
+        }
     });
 }
 
@@ -85,10 +101,16 @@ const askAgain = () => {
     }]).then(function(response) {
         let choice = response.continue.toLowerCase();
         if (choice === "y") {
-            question(); //replace with question() 
+            question();
         }
         else if(choice === "n") {
-            return;
+            x++; //moves on to next role to fill
+            if(x<3) {
+                question();
+            }
+            else if(x=3) {
+                return console.log(teamMembers);
+            }
         } 
         else if(choice !== "y" || choice !== "n") {
             console.log("Please input a correct character:");
@@ -97,15 +119,26 @@ const askAgain = () => {
     });
 }
 
-const startApp = () => { //not waiting for input
-    question();
-    x++;
-    question();
-    askAgain();
-    x++;
-    question();
-    askAgain();
-    //generate html
+const generateHTML = param1 => {
+    fs.writeFile()
+}
+
+const startApp = () => {
+    question().then(() => {
+        teamMembers.forEach(member => {
+            switch(member.getRole()) {
+                case "Manager":
+                    return generateHTML(member);
+                case "Engineer":
+                    return generateHTML(member);
+                case "Intern":
+                    return generateHTML(member);
+                default:
+                    return console.log("Something's gone wrong!");
+            }
+        });
+    });
+    //generate html from teamMembers array
 }
 
 startApp();
